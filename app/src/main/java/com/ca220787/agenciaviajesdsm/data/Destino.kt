8c9563886c3698e1833dc390597361cd42051e52
RoqueLovelace <chrisdesanfrancisco@gmail.com>
2026-03-27 19:27:14 -0600

    Update Destino.kt and ImgUtil.kt

--- app/src/main/java/com/ca220787/agenciaviajesdsm/data/Destino.kt
+++ app/src/main/java/com/ca220787/agenciaviajesdsm/data/Destino.kt
@@ -1,4 +1,10 @@
 package com.ca220787.agenciaviajesdsm.data
 
-class Destino {
-}
+data class Destino(
+    var idDest: String = "",
+    var nombDest: String = "",
+    var paisDest: String = "",
+    var precDest: Double = 0.0,
+    var descDest: String = "",
+    var imagBase: String = ""
+)
--- app/src/main/java/com/ca220787/agenciaviajesdsm/util/ImgUtil.kt
+++ app/src/main/java/com/ca220787/agenciaviajesdsm/util/ImgUtil.kt
@@ -1,4 +1,33 @@
 package com.ca220787.agenciaviajesdsm.util
 
+import android.content.ContentResolver
+import android.graphics.Bitmap
+import android.graphics.BitmapFactory
+import android.net.Uri
+import android.util.Base64
+import java.io.ByteArrayOutputStream
+
 class ImgUtil {
+    companion object {
+        fun uriABase64(contRes: ContentResolver, uriImag: Uri): String {
+            val inpStr = contRes.openInputStream(uriImag)
+            val bitMap = BitmapFactory.decodeStream(inpStr)
+            inpStr?.close()
+
+            val outStr = ByteArrayOutputStream()
+            bitMap.compress(Bitmap.CompressFormat.JPEG, 70, outStr)
+            val bytArr = outStr.toByteArray()
+
+            return Base64.encodeToString(bytArr, Base64.DEFAULT)
+        }
+
+        fun base64ABitmap(txtBase: String): Bitmap? {
+            return try {
+                val bytArr = Base64.decode(txtBase, Base64.DEFAULT)
+                BitmapFactory.decodeByteArray(bytArr, 0, bytArr.size)
+            } catch (e: Exception) {
+                null
+            }
+        }
+    }
 }
